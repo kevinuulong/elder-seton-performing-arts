d2a6f92dcc5e92d8fc8c54409c6b5fd799ccec98
Kevin Long <kevin.uu.long@gmail.com>
2020-11-16 21:54:34 -0500

    Basic Design implementation

---
+++ main.js
@@ -0,0 +1,17 @@
+function loadAct(act) {
+    document.getElementById("acts").style.display = "none";
+    document.getElementById("act-details").style.display = "block";
+    switch (act) {
+        case 0:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function hamburger() {
+    document.getElementById("acts").style.display = "block";
+    document.getElementById("act-details").style.display = "none";
+}
